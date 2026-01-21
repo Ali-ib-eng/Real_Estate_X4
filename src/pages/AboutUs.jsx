56288c20e2307f87da2ad2ec9c014430ea46c8fa
Lama Soliman <lama.soliman2023@gmail.com>
2026-01-21 14:20:46 +0100

    backup my work before pulling updates

--- src/pages/AboutUs.jsx
+++ src/pages/AboutUs.jsx
@@ -1,10 +1,13 @@
 import NavBar from "../componant/NavBar/NavBar"
+import Experience from "../componant/Experience/Experience";
+
 
 const AboutUs = () => {
   return (
     <div>
       <NavBar/>
       hello from AboutUs
+      <Experience/>
     </div>
   )
 }
